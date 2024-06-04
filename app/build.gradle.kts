a05e7441acada9b416f0da6bb310f3a8794015f9
hhrnjic1 <148076339+hhrnjic1@users.noreply.github.com>
2024-06-04 23:43:05 +0200

    ...

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,25 +37,6 @@ android {
 }
 
 dependencies {
-    /*implementation(libs.androidx.core)
-    implementation(libs.firebase.inappmessaging.ktx)
-    androidTestImplementation("androidx.test.espresso:espresso-contrib:3.5.1")
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    implementation ("androidx.core:core-ktx:1.6.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
-    implementation ("com.squareup.okhttp3:okhttp:4.9.3")
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation ("com.github.bumptech.glide:glide:4.12.0")
-    annotationProcessor ("com.github.bumptech.glide:compiler:4.12.0")*/
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
